fe231cb0f558cfacffa2081b5d309dd41a0ccc1d
burmisha <burmisha@gmail.com>
2013-08-20 17:04:02 +0100

    fix exception

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,19 +13,24 @@ import java.sql.*;
 public class Application extends Controller {
   
     public static Result index() {
-		URI dbUri = new URI(System.getenv("DATABASE_URL")); // 
-	    String username = dbUri.getUserInfo().split(":")[0];
-	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+    	try {
+			URI dbUri = new URI(System.getenv("DATABASE_URL")); // 
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-	    Connection connection = DriverManager.getConnection(dbUrl, username, password);
+			Connection connection = DriverManager.getConnection(dbUrl, username, password);
 
-	    // http://alvinalexander.com/java/edu/pj/jdbc/jdbc0003
- 		Statement stmt = connection.createStatement(); 
-        ResultSet rs = stmt.executeQuery("SELECT sum(value) FROM numbers;");
+			// http://alvinalexander.com/java/edu/pj/jdbc/jdbc0003
+				Statement stmt = connection.createStatement(); 
+			ResultSet rs = stmt.executeQuery("SELECT sum(value) FROM numbers;");
 
-		// http://www.javamex.com/tutorials/database/jdbc_result_set.shtml#.UhONtVlOWSo
-        return ok(index.render("Your new application is ready. Hey! " + rs.getString(1)));
+			connection.close();
+			// http://www.javamex.com/tutorials/database/jdbc_result_set.shtml#.UhONtVlOWSo
+			return ok(index.render("Your new application is ready. Hey! " + rs.getString(1)));
+        } catch (Exception e) {
+            return ok(index.render("Feeling bad! " + e.getMessage());
+        }
     }
   
 }
