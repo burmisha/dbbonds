7565845c1d1399b95542278da5fbaabc93c744c6
burmisha <burmisha@gmail.com>
2013-08-20 08:05:23 +0100

    fix to show

--- web.rb
+++ web.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
-require 'pg'
+# require 'pg'
 
 # conn = PGConn.open(:dbname => 'd98ldghnhunfae')
 # res = conn.exec('SELECT SUM(value) FROM numbers')
 # answer = res.getvalue(0, 0)
 
 get '/' do
-  "<h1>Hello, world! Specially for team 6.</h1> Sum in DB is " # + answer.to_s + "."
+  "<h1>Hello, world! Specially for team 6.</h1> Sum in DB is " # + answer.to_s + "." 
 end
